af4bf2638aa0547becf64dd5eed18095ea21d37c
Timothy Fredette <tim@unspecified.me>
2018-01-23 17:35:38 -0500

    find field by id over class name

--- tests/acc/features/steps/google_steps.py
+++ tests/acc/features/steps/google_steps.py
@@ -20,15 +20,10 @@ from behave import *
 	
 @when(u'I fill in the search field with "{value}"')
 def step_impl(context, value):
-	text_field = find_field_by_class(context.browser, 'gsfi')
+	text_field = context.browser.find_element_by_id("lst-ib")
 	text_field.clear()
 	text_field.send_keys(value)
 	
 @then(u'I should see "{result}" in the search results within 2 seconds')
 def step_impl(context, result):
 	raise NotImplementedError(u'STEP: Then I should see "unspecified.life" in the search results within 2 seconds')
-
-def find_field_by_class(browser, attribute):
-    xpath = "//input[@class='%s']" % attribute
-    elems = browser.find_elements_by_xpath(xpath)
-    return elems[0] if elems else False
