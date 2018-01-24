4d64d8a097145b762470681b807ade8511e97296
Timothy Fredette <tim@unspecified.me>
2018-01-24 14:35:28 -0500

    Update assertion to find link by text. Tell search field to press enter after entering search data

--- tests/acc/features/steps/google_steps.py
+++ tests/acc/features/steps/google_steps.py
@@ -17,14 +17,14 @@ Created on Jan 15, 2018
 	limitations under the License.
 '''
 from behave import * 
-	
+from selenium.webdriver.common.keys import Keys
+
 @when(u'I fill in the search field with "{value}"')
 def step_impl(context, value):
 	text_field = context.browser.find_element_by_id("lst-ib")
 	text_field.clear()
-	text_field.send_keys(value)
+	text_field.send_keys(value, Keys.ENTER)
 	
 @then(u'I should see "{result}" as the first result')
 def step_impl(context, result):
-	result_link = context.browser.find_element_by_css_selector("#rso > div > div > div:nth-child(1) > div > div > h3 > a")
-	assert result_link.href.contains(result)
+	assert len(context.browser.find_elements_by_link_text("Unspecified LLC")) > 0
